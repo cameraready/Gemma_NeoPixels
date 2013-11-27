0a5ad1a0370b8da28c1992f876d983de60251285
cameraready <ross.heironimus@gmail.com>
2013-11-26 22:04:47 -0500

    Update and rename NeoPixelHoops3.ino to NeoPixelHoops.ino
    One more time....

--- NeoPixelHoops.ino
+++ NeoPixelHoops.ino
@@ -13,22 +13,23 @@ uint8_t  mode   = 0, // Current animation effect
          offset = 0; // Position of spinny eyes
 uint32_t color  = 0xFFAE00; // Start color
 uint32_t prevTime;
+uint32_t arrows; // Define 3 steps for arrow animation
 
 void setup() {
   pixels.begin();
-  pixels.setBrightness(64); // 1/4 brightness
+  pixels.setBrightness(48); // brightness
   prevTime = millis();
 }
 
 void loop() {
   uint8_t  i;
   uint32_t t;
-  uint8_t  numPixOn = 3;
+  uint8_t  numPixOn = 2;
 
   switch(mode) {
 
    case 0: // Random sparks - just one LED on at a time!
-    i = random(NUMPIX);
+    i = random(NUMPIX*2); // 1/2 of pixels on?
     pixels.setPixelColor(i, color);
     pixels.show();
     delay(10);
@@ -38,19 +39,35 @@ void loop() {
    case 1: // Spinny wheels 
     for(i=0; i<NUMPIX; i++) {
       uint32_t c = 0;
-      if(((offset + i) & 7) < numPixOn) c = color; // use variable to set the number of pixels on during cycle
+      if(((offset + i) & 15) < numPixOn) c = color; // use variable to set the number of pixels on during cycle
       pixels.setPixelColor( i, c); 
     }
     pixels.show();
     offset++;
     delay(50);
     break;
+    
+    case 2: // Arrows animation
+      arrows = 0b010101000100100010101000100100;
+      uint32_t c = 0;
+      for(i=0; i<3; i++) {
+        for(uint8_t j=0; j<NUMPIX; j++) {
+          if (arrows&1)
+            c = 0x00FF00;
+          else
+            c = 0;
+          pixels.setPixelColor( j, c);  // Solid green color
+          arrows >>= 1;
+        }
+      pixels.show();
+      delay(150);
+      }
   }
 
   t = millis();
   if((t - prevTime) > 8000) {      // Every 8 seconds...
     mode++;                        // Next mode
-    if(mode > 1) {                 // End of modes?
+    if(mode > 2) {                 // End of modes?
       mode = 0;                    // Start modes over
       color >>= 8;                 // Next color R->G->B
       if(!color) color = 0xFFAE00; //0xFF0000; // Reset to color
